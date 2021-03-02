4999e6b54b40ed413e11c5389ca411574d36f95d
Austin Whitley <austinwhitley@Austins-MacBook-Pro.local>
2021-03-01 22:38:27 -0700

    Added More demensions to Room1 and flesh out Ranger class

--- main.py
+++ main.py
@@ -14,14 +14,36 @@ class door1:
     description = "A wooden door, with a 1 on it. The door has a glowing red light comming underneath it"
     class room1:
         description = "You walk into the room and find a red glowing orb engraved in ancient wrighting"
+        options = ["Take Orb", "Search for a way out"]
+class orbTaken:
+    description = "The orb glows with emense light, and they energy flows into your spirit"
+class searchForWayOut:
+    description = "While looking around the glowing room, you find a hole in the ground."
+
 class door2:
     description = "A stone door with a 2 on it. No light can be seen comming from it"
     class room2:
         description = "You walk into a room with that holds nothing, simply one giant room with no exits it seems. The door suddenly closes behind you."
+        options = ["Search for a way out", "Try to open that entrance"]
 class door3:
     description = "A beaten up door with a 3 on it. Their is no light coming from it, but you feel wind blowing through the cracks."
     class room3:
         description = "You walk into a black errie room. When you try to close the half destroyed door the doorway caves in and your left in the dark."
+        options = ["Allow to your eyes to adjust","Feel around for something"]
+
+def redOrbRoom():
+    redOrb = door1.room1
+    print("1:", redOrb.options[0])
+    print("2:", redOrb.options[1])
+    choise = int(input("1 or 2?"))
+    insertPrintBreaks()
+    if choise == 1:
+        choise1 = orbTaken
+        print(choise1.description)
+    elif choise == 2:
+        choise2 = searchForWayOut
+        print(choise2.description)
+
 
 def main():
     character = chooseYourCharacter()
@@ -37,6 +59,7 @@ def firstRoomChosen(doorChosen):
     if doorChosen == door1:
         print(door1.room1.description)
         insertPrintBreaks()
+        redOrbRoom()
     elif doorChosen == door2:
         print(door2.room2.description)
         insertPrintBreaks()
@@ -81,6 +104,7 @@ def originRoom():
         doorObject = door1
         print(doorObject.description)
         print("Would you like to go through the door?")
+        insertPrintBreaks()
         doorChoise1 = input("Yes or No")
         insertPrintBreaks()
         if doorChoise1 == "Yes":
@@ -114,4 +138,11 @@ def originRoom():
             print("Okay which door?")
             insertPrintBreaks()
             return originRoom()
+
+
+
+
+
+
+
 main()
